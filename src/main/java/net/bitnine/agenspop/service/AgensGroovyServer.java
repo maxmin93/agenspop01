7ecc4f18fc82e02e483dc870a2f14c30461d87ea
Byeong-Guk Min <maxmin93@gmail.com>
2019-07-16 15:00:32 +0900

    added scriptEngine

---
+++ src/main/java/net/bitnine/agenspop/service/AgensGroovyServer.java
@@ -0,0 +1,180 @@
+package net.bitnine.agenspop.service;
+
+import net.bitnine.agenspop.graph.AgensGraphManager;
+
+import org.apache.tinkerpop.gremlin.server.util.LifeCycleHook;
+import org.apache.tinkerpop.gremlin.server.util.MetricManager;
+import org.apache.tinkerpop.gremlin.server.util.ThreadFactoryUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.apache.tinkerpop.gremlin.groovy.engine.GremlinExecutor;
+import org.apache.tinkerpop.gremlin.jsr223.GremlinScriptEngine;
+import org.apache.tinkerpop.gremlin.process.traversal.TraversalSource;
+import org.apache.tinkerpop.gremlin.server.Channelizer;
+import org.apache.tinkerpop.gremlin.server.GraphManager;
+import org.apache.tinkerpop.gremlin.server.GremlinServer;
+import org.apache.tinkerpop.gremlin.server.Settings;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.script.SimpleBindings;
+import java.lang.reflect.Constructor;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.stream.Collectors;
+
+
+// groovy service
+//
+// **참고
+// https://github.com/eugenp/tutorials/blob/master/core-groovy-2/src/main/java/com/baeldung/MyJointCompilationApp.java
+
+@Service
+public class AgensGroovyServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(AgensGroovyServer.class);
+
+    private AgensGraphManager graphManager;
+    private Settings settings;
+    private List<LifeCycleHook> hooks;
+
+    private ScheduledExecutorService scheduledExecutorService;
+    private ExecutorService gremlinExecutorService;
+    private GremlinExecutor gremlinExecutor;
+
+    private final Map<String,Object> hostOptions = new ConcurrentHashMap<>();
+
+    @Autowired
+    public AgensGroovyServer(AgensGraphManager graphManager
+            , ExecutorService gremlinService, ScheduledExecutorService scheduledService) {
+        System.out.println("0) ======================================");
+
+        this.graphManager = graphManager;
+        this.gremlinExecutorService = gremlinService;
+        this.scheduledExecutorService = scheduledService;
+        this.settings = new Settings();
+
+        System.out.println("1) ======================================");
+        logger.info("Initialized Gremlin thread pool.  Threads in pool named with pattern gremlin-*");
+
+        final GremlinExecutor.Builder gremlinExecutorBuilder = GremlinExecutor.build()
+                .scriptEvaluationTimeout(settings.scriptEvaluationTimeout)
+                .afterFailure((b, e) -> this.graphManager.rollbackAll())
+                .beforeEval(b -> this.graphManager.rollbackAll())
+                .afterTimeout(b -> this.graphManager.rollbackAll())
+                .globalBindings(this.graphManager.getAsBindings())
+                .executorService(this.gremlinExecutorService)
+                .scheduledExecutorService(this.scheduledExecutorService);
+
+        settings.scriptEngines.forEach((k, v) -> {
+            // use plugins if they are present
+            if (!v.plugins.isEmpty()) {
+                // make sure that server related classes are available at init - new approach. the LifeCycleHook stuff
+                // will be added explicitly via configuration using GremlinServerGremlinModule in the yaml
+                gremlinExecutorBuilder.addPlugins(k, v.plugins);
+            }
+        });
+
+        gremlinExecutor = gremlinExecutorBuilder.create();
+
+        System.out.println("2) ======================================");
+        logger.info("Initialized GremlinExecutor and preparing GremlinScriptEngines instances.");
+
+        // force each scriptengine to process something so that the init scripts will fire (this is necessary if
+        // the GremlinExecutor is using the GremlinScriptEngineManager. this is a bit of hack, but it at least allows
+        // the global bindings to become available after the init scripts are run (DefaultGremlinScriptEngineManager
+        // runs the init scripts when the GremlinScriptEngine is created.
+        settings.scriptEngines.keySet().forEach(engineName -> {
+            try {
+                // use no timeout on the engine initialization - perhaps this can be a configuration later
+                final GremlinExecutor.LifeCycle lifeCycle = GremlinExecutor.LifeCycle.build().
+                        scriptEvaluationTimeoutOverride(0L).create();
+                gremlinExecutor.eval("1+1", engineName, new SimpleBindings(Collections.emptyMap()), lifeCycle).join();
+                registerMetrics(engineName);
+                logger.info("Initialized {} GremlinScriptEngine and registered metrics", engineName);
+            } catch (Exception ex) {
+                logger.warn(String.format("Could not initialize %s GremlinScriptEngine as init script could not be evaluated", engineName), ex);
+            }
+        });
+
+        System.out.println("3) ======================================");
+
+        // script engine init may have altered the graph bindings or maybe even created new ones - need to
+        // re-apply those references back
+        gremlinExecutor.getScriptEngineManager().getBindings().entrySet().stream()
+                .filter(kv -> kv.getValue() instanceof Graph)
+                .forEach(kv -> this.graphManager.putGraph(kv.getKey(), (Graph) kv.getValue()));
+
+        // script engine init may have constructed the TraversalSource bindings - store them in Graphs object
+        gremlinExecutor.getScriptEngineManager().getBindings().entrySet().stream()
+                .filter(kv -> kv.getValue() instanceof TraversalSource)
+                .forEach(kv -> {
+                    logger.info("A {} is now bound to [{}] with {}", kv.getValue().getClass().getSimpleName(), kv.getKey(), kv.getValue());
+                    this.graphManager.putTraversalSource(kv.getKey(), (TraversalSource) kv.getValue());
+                });
+
+        System.out.println("4) ======================================");
+
+        // determine if the initialization scripts introduced LifeCycleHook objects - if so we need to gather them
+        // up for execution
+        hooks = gremlinExecutor.getScriptEngineManager().getBindings().entrySet().stream()
+                .filter(kv -> kv.getValue() instanceof LifeCycleHook)
+                .map(kv -> (LifeCycleHook) kv.getValue())
+                .collect(Collectors.toList());
+
+        System.out.println("5) ======================================");
+    }
+
+    private void registerMetrics(final String engineName) {
+        final GremlinScriptEngine engine = gremlinExecutor.getScriptEngineManager().getEngineByName(engineName);
+        MetricManager.INSTANCE.registerGremlinScriptEngineMetrics(engine, engineName, "sessionless", "class-cache");
+    }
+
+    public void addHostOption(final String key, final Object value) {
+        hostOptions.put(key, value);
+    }
+
+    public Map<String,Object> getHostOptions() {
+        return Collections.unmodifiableMap(hostOptions);
+    }
+
+    public Object removeHostOption(final String key) {
+        return hostOptions.remove(key);
+    }
+
+    public void clearHostOptions() {
+        hostOptions.clear();
+    }
+
+    public ScheduledExecutorService getScheduledExecutorService() {
+        return scheduledExecutorService;
+    }
+
+    public GremlinExecutor getGremlinExecutor() {
+        return gremlinExecutor;
+    }
+
+    public ExecutorService getGremlinExecutorService() {
+        return gremlinExecutorService;
+    }
+
+    public GraphManager getGraphManager() {
+        return graphManager;
+    }
+
+    public Settings getSettings() {
+        return settings;
+    }
+
+    public List<LifeCycleHook> getHooks() {
+        return hooks;
+    }
+}
