c2db1576172e5556e00056a9752cc587f2b9b158
maxmin93 <maxmin93@gmail.com>
2020-02-23 22:22:53 +0900

    add BrowserAnimationsModule but still error in NgxSpinner

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -41,6 +43,8 @@ import { GcFocusComponent } from './workspace/canvas/components/gcfocus/gcfocus.
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    
     NgbModule,
     FormsModule,
     AppRoutingModule,
